9f9ce67de0f0350796bc1412bf8dc42c6dcbfeef
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2024-08-06 11:17:22 -0300

    feat: add main file

---
+++ main.py
@@ -0,0 +1,21 @@
+
+from controller import Controller
+from models import ProductFactory
+from views import CLIView
+
+
+def main():
+    from repositories import JsonProductRepository
+
+    repository_options = {
+        "filename": "products.json",
+    }
+    repository = JsonProductRepository(**repository_options)
+    view = CLIView()
+    product_factory = ProductFactory()
+    controller = Controller(repository=repository, view=view, product_factory=product_factory)
+    controller.run()
+
+
+if __name__ == "__main__":
+    main()
