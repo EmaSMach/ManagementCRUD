aae858de342921d630a1ba144b73965c16053b47
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2024-10-07 01:32:30 -0300

    feat: switch to mysql repository

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+from decouple import config
+
 from controller import Controller
+from db import TABLES, MySqlConnector
 from models import ProductFactory
+from repositories import MySQLProductRepository
 from views import CLIView
 
 
 def main():
-    from repositories import JsonProductRepository
-
-    repository_options = {
-        "filename": "products.json",
+    connector_options = {
+        "conf": config,
+        "table_definitions": TABLES,
     }
-    repository = JsonProductRepository(**repository_options)
+    connector = MySqlConnector(**connector_options)
+    connector.create_database(config("DB_NAME"))  # Create database if it doesn't exist
+    connector.create_tables()  # Create tables if they don't exist
+    repository = MySQLProductRepository(connector)
     view = CLIView()
     product_factory = ProductFactory()
     controller = Controller(repository=repository, view=view, product_factory=product_factory)
