a7a52655971ec21ddec2ee7be66c2a689c9326cb
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2024-10-07 01:43:38 -0300

    feat: add loggers

---
+++ loggers.py
@@ -0,0 +1,19 @@
+import logging
+
+# logger = logging.getLogger(__name__)
+logger = logging.getLogger("DATABASE_LOGGER")
+
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+# Log to console
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Also log to a file
+file_handler = logging.FileHandler("cpy-errors.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+__all__ = ["logger"]
